c5d5aa437fd1cb9a9b447955675fa0514524391d
John Schmidley <tlg8n@outlook.com>
2019-09-20 23:38:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,24 +11,7 @@ end
 end
 
 def take_a_number(katzDeliLine, nowServing)
-   let i = 0;
-  while (i < katzDeli.length) {
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "There is nobody waiting to be served!";
-  }
-  else
-  return (`Currently serving ${katzDeli.shift()}.`);
-}
 
-// Build a function currentLine that returns the current line.
-// For example, if katzDeliLine is currently ["Ada", "Grace"],
-// currentLine(katzDeliLine) would return "The line is currently: 1. Ada 2. Grace".
-// If there is nobody in line, it should return "The line is currently empty."
-
-
-var line = [];
 
 end
 
