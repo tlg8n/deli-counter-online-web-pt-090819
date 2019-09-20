3da645f5b538fd9dd64525b84efc0895465e33fc
John Schmidley <tlg8n@outlook.com>
2019-09-20 23:34:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def line(deliline)
+  if deliline.empty?
+  puts "The line is currently empty."
+else current_line= "The line is currently:"
+  deliline.each.with_index(1) do |person, i| 
+    current_line << " #{i}. #{person}"
+  end
+  puts current_line
+end
+end
+
+def take_a_number(katzDeliLine, nowServing)
+  
+
+end
+
