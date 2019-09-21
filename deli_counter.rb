4a05e199676e2ad2a57cd90003a92ac7d2cd64e7
John Schmidley <tlg8n@outlook.com>
2019-09-21 00:20:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,6 @@ end
 
 def take_a_number(array,name)
   array << name
-  puts "welcome, #{name}. You are number #{array.length} in line"
+  puts "welcome, #{name}. You are number #{array.length} in line."
 end
 
